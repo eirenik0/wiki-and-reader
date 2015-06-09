35964a6004fbe7aacc4a33078eda73f2ccc4b2fe
Serhii Khalymon <sergiykhalimon@gmail.com>
2015-06-09 12:36:26 +0300

    fix error with login

--- wiki_allatra_club/users/admin.py
+++ wiki_allatra_club/users/admin.py
@@ -1,14 +1,33 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from django import forms
 from django.contrib import admin
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
 from .models import User
 
 
+class MyUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError(self.error_messages['duplicate_username'])
+
+
+@admin.register(User)
 class UserAdmin(AuthUserAdmin):
-    create_form_class = UserCreationForm
-    update_form_class = UserChangeForm
-
-
-admin.site.register(User, UserAdmin)
+    form = MyUserChangeForm
+    add_form = MyUserCreationForm
