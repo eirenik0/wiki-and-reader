84bc6c4103ff0ce74e5ac1a430be479e957b73a5
Serhii Khalymon <sergiykhalimon@gmail.com>
2015-06-15 18:08:45 +0300

    change order of books

--- wiki_allatra_club/context_processors/__init__.py
+++ wiki_allatra_club/context_processors/__init__.py
@@ -2,5 +2,5 @@ from wiki_allatra_club.reader.models import BookModel
 
 def get_books(request):
     context = {}
-    context['books_list'] = BookModel.objects.all()
+    context['books_list'] = BookModel.objects.all().order_by('title')
     return context
