15c707fe72f749256c055ca9682f73b1413c4f9a
Serhii Khalymon <sergiykhalimon@gmail.com>
2015-12-31 01:52:09 +0200

    added timeout to cache

--- config/settings/production.py
+++ config/settings/production.py
@@ -44,6 +44,10 @@ CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
         'LOCATION': '/var/tmp/django_cache',
+        'TIMEOUT': 60,
+        'OPTIONS': {
+            'MAX_ENTRIES': 1000
+        }
     }
 }
 
